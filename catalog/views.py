4c2991773134f1ba2fec853948d9615c38dbd0a9
Sadauchi <kysnez4@gmail.com>
2025-06-09 22:05:29 +0300

    update

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +6,11 @@ def home(request):
     return render(request, 'catalog/home.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        email = request.POST.get('email')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'catalog/contact.html')
 
 def catalog(request):
@@ -12,3 +18,4 @@ def catalog(request):
 
 def category(request):
     return render(request, 'catalog/category.html')
+
