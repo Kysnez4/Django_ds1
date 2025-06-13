5aa60bca462fd87f95248b67c059e357c147795a
Sadauchi <kysnez4@gmail.com>
2025-06-13 13:05:05 +0300

    update pattern

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('contacts/', views.contact, name='contact'),
     path('category/', views.category, name='category'),
     path('catalog/', views.catalog, name='catalog'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('product/add/', views.add_product, name='add_product'),
 ]
